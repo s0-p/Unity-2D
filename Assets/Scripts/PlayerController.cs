af0993fddb425f26aa5bad1abe370485fc58a737
s0 <b00v0429@gmail.com>
2023-05-12 19:59:46 +0900

    RigidBody 2D, Box Collider 2D

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Animator m_animator;
+    [SerializeField] SpriteRenderer m_spriteRenderer;
+    [SerializeField] float m_speed = 10;
+    Rigidbody2D m_rigidbody2D;
+    Vector3 m_dir;
+
     public int HP { get; set; }
     private void Awake()
     {
@@ -19,7 +25,13 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        m_animator = gameObject.GetComponent<Animator>();
+        m_spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        m_rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        //m_rigidbody2D.velocity += (Vector2) m_dir * m_speed * Time.deltaTime;
     }
 
     // Update is called once per frame
@@ -28,25 +40,48 @@ public class PlayerController : MonoBehaviour
         ActionControl();
     }
 
+    
     void ActionControl()
     {
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+        //    for (int i = 0; i < 2000; i++)
+        //    {
+        //        var obj = Instantiate(gameObject);
+        //        obj.transform.position = new Vector3(Random.Range(-9f, 9f), Random.Range(-5f, 5f));
+        //    }
+        //}
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            HP -= 10;
-            Debug.Log("남은 체력 : " + HP);
-            if (HP <= 0)
-            {
-                Debug.Log("플레이어 사망!");
-                gameObject.SetActive(false);
-            }
+            m_animator.SetBool("IsFire", true);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            m_animator.SetBool("IsFire", false);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            m_animator.SetBool("IsMove", false);
+            m_dir = Vector3.zero;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            gameObject.transform.position += Vector3.left * 0.01f;
+            //gameObject.transform.position += Vector3.left * 0.01f;
+            m_dir = Vector3.left;
+            m_animator.SetBool("IsMove", true);
+            m_spriteRenderer.flipX = false;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            gameObject.transform.position += Vector3.right * 0.01f;
+            //gameObject.transform.position += Vector3.right * 0.01f;
+            m_dir = Vector3.right;
+            m_animator.SetBool("IsMove", true);
+            m_spriteRenderer.flipX = true;
+        }
+        var stateInfo = m_animator.GetCurrentAnimatorStateInfo(0);
+        if (!stateInfo.IsName("Fire"))
+        {
+            gameObject.transform.position += m_dir * m_speed * Time.deltaTime;
         }
     }
 }
